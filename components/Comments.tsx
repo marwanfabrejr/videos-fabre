a9d6afb3635cc57025f0849bf9533c80a3d0f4be
Marwan Zayed <marwanzayed4444@gmail.com>
2023-03-05 16:19:18 +0200

    add id

--- components/Comments.tsx
+++ components/Comments.tsx
@@ -55,7 +55,7 @@ const Comments = ({
 
                           <p className='flex cursor-pointer gap-1 items-center text-[18px] font-bold leading-6 text-primary'>
                             {user.userName}{' '}
-                            <GoVerified className='text-[#009bec]' />
+                            <GoVerified className='text-blue-400' />
                           </p>
                         </div>
                       </Link>
@@ -78,12 +78,12 @@ const Comments = ({
           <form onSubmit={addComment} className='flex gap-4'>
             <input
               value={comment}
-              onChange={(e) => setComment(e.target.value)}
-              placeholder='Add comment...'
+              onChange={(e) => setComment(e.target.value.trim())}
               className='bg-primary px-6 py-4 text-md font-medium border-2 w-[250px] md:w-[700px] lg:w-[350px] border-gray-100 focus:outline-none focus:border-2 focus:border-gray-300 flex-1 rounded-lg'
+              placeholder='Add comment..'
             />
-            <button className='text-md text-gray-400' onClick={addComment}>
-              {isPostingComment ? 'Commenting' : 'Comment'}
+            <button className='text-md text-gray-400 ' onClick={addComment}>
+              {isPostingComment ? 'Commenting...' : 'Comment'}
             </button>
           </form>
         </div>
